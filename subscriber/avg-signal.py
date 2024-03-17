1bd922baee42ac3a4fc9e7306a1e574c83bcc339
GujralNaman <gujralnaman2002@gmail.com>
2024-03-17 14:43:28 +0530

    new file added

--- subscriber/avg-signal.py
+++ subscriber/avg-signal.py
@@ -1,23 +1,16 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import hour, avg, col
+from pyspark.sql.functions import *
 
-# Create SparkSession
 spark = SparkSession.builder.getOrCreate()
-
-# Read data from Delta Lake
 delta_df = spark.read.format("delta").load("delta_table")
 
-# Extract hour from signal_ts
-delta_df = delta_df.withColumn("hour", hour("signal_ts"))
+delta_table = delta_df.withColumn("date-hour", date_format(col('signal_ts'), "yyyy-MM-dd HH"))
 
-# Group by hour and calculate average for each signal
-avg_signals_per_hour = delta_df.groupby("hour").agg(
-    avg(col("LV_ActivePower")).alias("avg_LV_ActivePower"),
-    avg(col("Wind_Speed")).alias("avg_Wind_Speed"),
-    avg(col("Theoretical_Power_Curve")).alias("avg_Theoretical_Power_Curve"),
-    avg(col("Wind_Direction")).alias("avg_Wind_Direction")
+avg_signals_per_hour = delta_df.groupby("date-hour").agg(
+    avg(col["signals.LV_ActivePower"]).alias("avg_LV_ActivePower"),
+    avg(col["signals.Wind_Speed"]).alias("avg_Wind_Speed"),
+    avg(col["signals.Theoretical_Power_Curve"]).alias("avg_Theoretical_Power_Curve"),
+    avg(col["signals.Wind_Direction"]).alias("avg_Wind_Direction")
 )
 
-
-# Show the result
 avg_signals_per_hour.show()
